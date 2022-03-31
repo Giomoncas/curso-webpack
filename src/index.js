4f1f88e8c079563dae24e737e9617089fadfbf01
Giovanni Montoya <giovanni87070665@hotmail.com>
2022-03-31 17:55:54 -0500

    Curso inicial de como funciona webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import sum from './utils/sum.js';  //Importa la funcion sum de utils
+
+const hello = "hello";
+
+console.log(hello);
+console.log(sum(2,2));
---
+++ src/utils/sum.js
@@ -0,0 +1,5 @@
+const sum = (a,b) => {  //Funcion flecha que suma dos numeros
+    return a + b;
+}
+
+export default sum;  //Poder exportar la funcion sum
